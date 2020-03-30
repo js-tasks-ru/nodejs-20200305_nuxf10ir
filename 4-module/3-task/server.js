939b743a8d02bc74fa4e4bde154b6a63c13441dc
Igor Surgaev <i.surgaev@bi.zone>
2020-03-30 20:37:02 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
 
 const server = new http.Server();
 
@@ -9,8 +11,26 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const {dir} = path.parse(pathname);
+
+
   switch (req.method) {
     case 'DELETE':
+
+      if (!!dir) {
+        res.statusCode = 400;
+        res.end('Is Folder');
+        return;
+      }
+
+      fs.unlink(filepath, (err) => {
+        if (err && err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not exists');
+        }
+        res.statusCode = 200;
+        res.end('Deleted');
+      });
 
       break;
 
